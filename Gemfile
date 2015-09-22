447b79624f6bcf8f05d9d3368aa0405653039367
vestige <vestige_sync@me.com>
2015-09-22 23:13:42 +0900

    Finish user signup

--- Gemfile
+++ Gemfile
@@ -26,4 +26,5 @@ end
 group :test do
   gem 'selenium-webdriver', '2.35.1'
   gem 'capybara', '2.1.0'
+  gem 'factory_girl_rails', '4.2.1'
 end
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do 
+  factory :user do
+    name "YOYOYOYO"
+    email "yoyoyo@emai.com"
+    password "pass12345"
+    password_confirmation "pass12345"
+  end
+end
