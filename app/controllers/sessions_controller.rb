71a341681b2088b46218ad6d7f5078ba410c5e1d
vestige <vestige_sync@me.com>
2015-12-22 21:27:14 +0900

    add session

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if (user && user.authenticate(params[:session][:password]))
+    else
+      flash.now[:error] = 'Invalid email/passoword combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+end
